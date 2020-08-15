de9d9ddc62bc1b24e567f93a2379c107ee03ab46
sireesha <40299790+sireesha-p23@users.noreply.github.com>
2020-08-15 18:50:30 +0530

    Update Circle.java

--- week1/Circle.java
+++ week1/Circle.java
@@ -13,6 +13,9 @@ public class Circle{
 	void calculateAreaOfCircle(int Radius)
 	{
 		System.out.println(3.14*Radius*Radius);
+		/*changes made by person 1 */
+                System.out.println("Area of circle");
+
 	}
 
 }
